989412843bc65fc42d18c1eb361cac0a6de7adb7
CirnoBreak <1103054869@qq.com>
2021-03-05 16:51:48 +0800

    ci: public path change

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Blog',
   mode: 'site',
+  publicPath: '/blog/',
   locales: [['zh-CN', '中文']],
   // more config: https://d.umijs.org/config
 });
